5630cef851a0bdbb2417217e09eef2ad94f0e626
carlos pungu <pungucarlos@gmail.com>
2021-04-26 21:08:38 +0200

    my first project ionic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,35 @@
 <template>
   <ion-page>
     <ion-header :translucent="true">
+      <ion-toolbar class="pl" color="secondary">
+        <ion-title>Carlos-App</ion-title>
+      </ion-toolbar>
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+            <ion-searchbar color="primary"></ion-searchbar>
       </ion-toolbar>
     </ion-header>
     
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
-        <ion-toolbar>
+        <ion-toolbar >
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <div class="cible"><span></span></div>
+      <ion-item>
+            <ion-label>Nom:</ion-label>
+            <ion-input placehoder="your name"></ion-input>
+      </ion-item>
+        <ion-item>
+        <ion-label>prenom:</ion-label>
+            <ion-input placehoder="your name"></ion-input>
+        </ion-item>
+           <ion-item>
+           <ion-label>postnom:</ion-label><br>
+            <ion-input placehoder="your" expand="block"></ion-input>
+          </ion-item>
+        <ion-button >Valider</ion-button>
+
     </ion-content>
   </ion-page>
 </template>
@@ -38,31 +51,13 @@ export default defineComponent({
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+.cible{
+  width:20px;
+  background-color:red;
+  height:10px;
+  position:absolute;
+  top:100px;
+  right:200px;
+  z-index:5;
 }
 </style>
